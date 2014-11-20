09dc707be4dd1ff2772e626dbd02bee0533fb1df
Radek Vais <vaisr@students.zcu.cz>
2014-11-20 13:52:31 +0100

    posun dat z indexu do aplikace

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-    global $data;
+    /** @var data vypsani do sablony $data */
+    $data = array();
 
     session_start();
 
@@ -35,45 +36,8 @@
 
 		// nebo q = pozadovane url, ktere jsem dostal z .htaccess
 
+    $app->testDat();
 
-        $data["nadpis"]="Blábol";
-        $data["obsah"]='   <p class="text-justify">šablonou ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales arcu non fermentum vestibulum. Sed sed cursus risus. Donec porta urna in tellus sodales, ut congue velit blandit. In porttitor vulputate enim, vel viverra nulla mattis eu. Fusce mollis, diam egestas fringilla lobortis, tellus erat sodales ipsum, vitae auctor arcu lectus nec justo. Sed rhoncus, ex in condimentum rhoncus, velit est ultricies urna, sed posuere mauris lectus ac dui. Nullam tincidunt ligula nec congue commodo. Praesent pellentesque luctus pharetra. Maecenas at blandit nisi. Etiam vitae nulla lectus. Quisque sed augue elementum nisl tincidunt vulputate nec a est.
-                    </p>
-                    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales arcu non fermentum vestibulum. Sed sed cursus risus. Donec porta urna in tellus sodales, ut congue velit blandit. In porttitor vulputate enim, vel viverra nulla mattis eu. Fusce mollis, diam egestas fringilla lobortis, tellus erat sodales ipsum, vitae auctor arcu lectus nec justo. Sed rhoncus, ex in condimentum rhoncus, velit est ultricies urna, sed posuere mauris lectus ac dui. Nullam tincidunt ligula nec congue commodo. Praesent pellentesque luctus pharetra. Maecenas at blandit nisi. Etiam vitae nulla lectus. Quisque sed augue elementum nisl tincidunt vulputate nec a est.
-                    </p>
-                    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales arcu non fermentum vestibulum. Sed sed cursus risus. Donec porta urna in tellus sodales, ut congue velit blandit. In porttitor vulputate enim, vel viverra nulla mattis eu. Fusce mollis, diam egestas fringilla lobortis, tellus erat sodales ipsum, vitae auctor arcu lectus nec justo. Sed rhoncus, ex in condimentum rhoncus, velit est ultricies urna, sed posuere mauris lectus ac dui. Nullam tincidunt ligula nec congue commodo. Praesent pellentesque luctus pharetra. Maecenas at blandit nisi. Etiam vitae nulla lectus. Quisque sed augue elementum nisl tincidunt vulputate nec a est.
-                    </p>
-                    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales arcu non fermentum vestibulum. Sed sed cursus risus. Donec porta urna in tellus sodales, ut congue velit blandit. In porttitor vulputate enim, vel viverra nulla mattis eu. Fusce mollis, diam egestas fringilla lobortis, tellus erat sodales ipsum, vitae auctor arcu lectus nec justo. Sed rhoncus, ex in condimentum rhoncus, velit est ultricies urna, sed posuere mauris lectus ac dui. Nullam tincidunt ligula nec congue commodo. Praesent pellentesque luctus pharetra. Maecenas at blandit nisi. Etiam vitae nulla lectus. Quisque sed augue elementum nisl tincidunt vulputate nec a est.
-                    </p>';
-
-        $data["footer_left_a"]="http://www.pilirionos.org/pro-verejnost";
-        $data["footer_left"]="Co je to larp?";
-
-        $data["footer_right_a"]="http://www.pilirionos.org/";
-        $data["footer_right"]="Pilirion o.s.";
-
-
-        $data["menu"][0]["text"]="Akce";
-      //$data["menu"][0]["href"]="";
-
-        $data["menu"][1]["text"]="Hry";
-        //$data["menu"][1]["href"]="";
-
-        $data["menu"][2]["text"]="Letošní ročník";
-        //$data["menu"][2]["href"]="";
-
-        $data["menu"][3]["text"]="Registrace";
-        //$data["menu"][3]["href"]="";
-
-
-        $data["navbar"][3]["text"]="Prvnu bod";
-        //$data["menu"][3]["href"]="";
-
-
-        $data["navbar"][2]["text"]="druhy bod";
-        //$data["menu"][2]["href"]="";
-
-        $data["navbar_last"]="Aktualni stranka";
 
         Twig_Autoloader::register();
 
