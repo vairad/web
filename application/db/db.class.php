409ac0eac0a8fcd158a5349997949f11ed597ac3
Radek Vais <vaisr@students.zcu.cz>
2015-01-14 21:50:39 +0100

    pgsql connect

--- application/db/db.class.php
+++ application/db/db.class.php
@@ -552,7 +552,7 @@ class db
 	function Connect()
 	{
 		// PDO - MySQL
-		try
+		if(DB_TYPE == 'mysql')try
 		{
 			$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',);
 			$this->connection = new PDO("mysql:host=".DB_HOST.";dbname=".DB_DATABASE_NAME."", DB_USER_LOGIN, DB_USER_PASSWORD, $options);
@@ -560,6 +560,20 @@ class db
 			// nastavit pripojeni na UTF-8 - pro starsi verze PHP
 			//$this->connection->exec("SET NAMES UTF8");
 	
+		} catch (PDOException $e)
+		{
+			//print "Error!: " . $e->getMessage() . "<br/>";
+			die();
+		}
+
+		// PDO - MySQL
+		if(DB_TYPE == 'pgsql')try
+		{
+			$this->connection = new PDO("pgsql:host=".DB_HOST."; dbname=".DB_DATABASE_NAME."; user=".DB_USER_LOGIN."; password=". DB_USER_PASSWORD, $options);
+
+			// nastavit pripojeni na UTF-8 - pro starsi verze PHP
+			//$this->connection->exec("SET NAMES UTF8");
+
 		} catch (PDOException $e)
 		{
 			//print "Error!: " . $e->getMessage() . "<br/>";
