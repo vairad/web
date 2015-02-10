1adec1d1d21219c10c9313ce42bafddd507499ca
Radek Vais <vaisr@students.zcu.cz>
2015-02-10 11:34:26 +0100

    Update texts.inc.php

--- conf/texts.inc.php
+++ conf/texts.inc.php
@@ -4,21 +4,42 @@ $rights_text = " Dávám souhlas s tím, aby Pilirion o.s., Psohlavců 10, Praha
 
 $text_pivko = '<p class="text-justify h4">Ač by se mohlo zdát že jde o pozvánkou na alkoholický nápoj do Plzně, nenechte se zmást! Plzeňský Inspirační Víkendový Kurz vám může nabídnout zcela nový rozměr zážitků.
                  </p>
-                <p class="text-justify h4">Známé i neznámé komorní larpy, víkend strávený v Plzni ve společnosti larpových nadšenců a nabitý večerní offprogram. Přijeďte si do Plzně hrát!
+                <p class="text-justify h4">Víkendový festival komorních larpů vám nabídne několik bloků známých i neznámých her, čas strávený v Plzni ve společnosti larpových nadšenců a nabitý večerní offprogram. Přijeďte si do Plzně hrát!
                  </p>
                 <p class="text-justify h4">A že nevíte co je to larp? Láká vás poznat novou zábavu, spojenou s divadlem, fantasy, sci-fi či historií? Pak je PIVKo 12° akce přesně pro vás!
                  </p>';
 
-$text_o_akci = '   <p class="text-justify">PIVKo Dvanáctka je akce pro všechny, kdo mají odvahu a chuť přijít si hrát. Čeká vás víkend plný komorních larpů a dalšího doplňkového programu, kde poznáte spoustu lidí kteří se věnují larpu.
-Nevíte-li co to larp je, stačí použít odkaz na konci stránky.</p>
-                    <p class="text-justify">PIVKo Dvanáctka nemá pevně daný program, víkend je sestaven z několika bloků ve kterých si každý může (a nemusí) navolit hru, jaká se mu zamlouvá. Je tak možné sestavit si nabitý víkend a jít z larpu do larpu, stejně jako strávit pohodové tři dny ve ketrých si zahrajete jeden dva larpy a zbytek strávíte prohlídkou města či posezením s kamarády. Jedná se tedy o klasický formát larpového festivalu.
-                    </p>';
+$text_o_akci = ' <p><strong>PIVKo Dvan&aacute;ctka </strong>je akce pro v&scaron;echny, kdo maj&iacute; odvahu a chuť přij&iacute;t si hr&aacute;t. Ček&aacute; v&aacute;s v&iacute;kend pln&yacute; komorn&iacute;ch larpů a dal&scaron;&iacute;ho doplňkov&eacute;ho programu, kde pozn&aacute;te spoustu lid&iacute; kteř&iacute; se věnuj&iacute; larpu.</p>
+<p><strong>Nev&iacute;te-li co to larp je, stač&iacute; se pod&iacute;vat např&iacute;klad sem: </strong><a href="http://www.pilirionos.org/pro-verejnost" target="_blank">Pilirion - Pro veřejnost</a></p>
+<p><strong>PIVKo Dvan&aacute;ctka </strong>je form&aacute;tem klasick&yacute; larpov&yacute; festival. Nem&aacute; tedy pevně dan&yacute; program, v&iacute;kend je sestaven z několika bloků ve kter&yacute;ch si každ&yacute; může (a nemus&iacute;) navolit hru, jak&aacute; se mu zamlouv&aacute;. Je tak možn&eacute; sestavit si nabit&yacute; v&iacute;kend a j&iacute;t z larpu do larpu, stejně jako str&aacute;vit pohodov&eacute; tři dny ve kter&yacute;ch si zahrajete jeden dva larpy a zbytek str&aacute;v&iacute;te prohl&iacute;dkou města či posezen&iacute;m s kamar&aacute;dy.</p>
+<p><strong>Akci PIVKo již p&aacute;t&yacute;m rokem poř&aacute;d&aacute; neziskov&yacute; spolek Pilirion o. s.</strong> (<a href="http://www.pilirionos.org/"><strong>Web</strong></a>, <a href="https://www.facebook.com/pilirion?fref=ts"><strong>Facebook</strong></a>)</p>
+<h3><strong>Organiz&aacute;torsk&yacute; t&yacute;m akce tvoř&iacute;:</strong></h3>
+<p><strong>Jiř&iacute; &ldquo;Pilly&rdquo; Zahradn&iacute;k </strong></p>
+<p><strong>Magda &ldquo;Hro&scaron;e&rdquo; &Ouml;rd&ouml;gov&aacute; </strong></p>
+<p><strong>Jakub &ldquo;Aki&rdquo; Matas </strong></p>
+<p><em>(vrchn&iacute; organiz&aacute;torsk&eacute; trio)</em></p>
+<p><strong>Radek &ldquo;Olie&rdquo; Vais </strong></p>
+<p><strong>Pavel &ldquo;Tkaj&rdquo; Tomiczek </strong></p>
+<p><em>(IT z&aacute;zem&iacute; a design)</em></p>
+<p><strong>Radek &ldquo;Rado&rdquo; H&uuml;bl </strong></p>
+<p><em>(komunikace s organiz&aacute;tory, St&iacute;n Magdalenin)</em></p>
+<h4><strong>Poděkov&aacute;n&iacute; nad&aacute;le patř&iacute; n&aacute;sleduj&iacute;c&iacute;m:</strong></h4>
+<p><strong>Luk&aacute;&scaron; &ldquo;Finmer&rdquo; Beran, Jan &ldquo;Zagro&rdquo; Zahradn&iacute;k, Jakub &ldquo;Balda&rdquo; Balhar, Danča Kojzarov&aacute;, Lenka &ldquo;Alienka&rdquo; Pittnerov&aacute;, Jakub &ldquo;Kořen&rdquo; Korbel, Lenka Žaitlikov&aacute;, Jaroslav &ldquo;Roslav&rdquo; Voves, Karel &ldquo;Keret&rdquo; Ř&iacute;ha, Luk&aacute;&scaron; &ldquo;Viper&rdquo; Makovička, a dal&scaron;&iacute;, kter&yacute;m moc děkujeme za obs&aacute;hlou podporu a pomoc!</strong></p>';
 
 $text_letos = '<h4>27.2. - 1.3.2015</h4>
-<p>Program PIVKa bude již tradičně zač&iacute;nat v p&aacute;tek v odpoledn&iacute;ch hodin&aacute;ch<strong> registrac&iacute; </strong>a <strong>několika m&aacute;lo larpy </strong>a jinou společenskou z&aacute;bavou. Od půl des&aacute;t&eacute; si budete moct na klubovně Seneč&aacute;k poslechnout koncert larpov&eacute;ho muzikanta<strong> Laega</strong>.</p>
-<p>V sobotu na v&aacute;s budou čekat <strong>dva hern&iacute; bloky, </strong>mezi kter&yacute;mi budete m&iacute;t dostatek času na oběd. <strong>Na večer je napl&aacute;novan&aacute; hromadn&aacute; party, </strong>letos v centru města v klubu <strong>U Star&eacute;ho psa (<a href="https://www.facebook.com/groups/53602423465/?fref=ts" target="_blank">Facebook</a>). </strong></p>
-<p>V neděli se můžete &uacute;častnit posledn&iacute;ho, <strong>dopoledn&iacute;ho bloku larpů</strong> a pot&eacute; dle libosti vyrazit domů.</p>
-<p><br /><strong>Dal&scaron;&iacute; informace ohledně leto&scaron;n&iacute;ho PIVKa zde přibydou v průběu &uacute;nora.</strong></p>
+<p>Co můžete od leto&scaron;n&iacute;ho ročn&iacute;ku oček&aacute;vat?</p>
+<p>Program PIVKa bude již tradičně zač&iacute;nat v p&aacute;tek v odpoledn&iacute;ch hodin&aacute;ch<strong> registrac&iacute;.</strong></p>
+<p><strong>P&aacute;tečn&iacute; REGISTRACE bude prob&iacute;hat na Totemu od 15.30 do 20.00. </strong>Z&aacute;roveň si zde budete moct odložit v&scaron;echny sv&eacute; věci, ve vr&aacute;tnici bude po celou dobu akce př&iacute;tomen dozor (buď vr&aacute;tn&aacute; Totemu nebo někdo od n&aacute;s).</p>
+<p>Ti kteř&iacute; doraz&iacute; později nebo se nestihnou dostavit na Totem před zač&aacute;tkem her, budou m&iacute;t je&scaron;tě možnost <strong>zaregistrovat se tamt&eacute;ž r&aacute;no mezi 7.00 - 8.00.</strong></p>
+<p><strong>Po zbytek sobotn&iacute;ho dne bude registrace prob&iacute;hat v kav&aacute;rně a čajovně Seraf v centru, kde bude z&aacute;roveň celou sobotu př&iacute;tomn&yacute; někdo z organiz&aacute;torů.</strong></p>
+<p><strong>V neděli se již s registrac&iacute; nepoč&iacute;t&aacute;. </strong>Pokud pl&aacute;nujete dorazit pouze na neděln&iacute; hru, ozvěte se n&aacute;m a domluv&iacute;me se individu&aacute;lně.</p>
+<p>V sobotu na v&aacute;s budou čekat <strong>dva hern&iacute; bloky, </strong>mezi kter&yacute;mi budete m&iacute;t dostatek času na oběd.</p>
+<p>Na každ&eacute; hře budou samozřejmě k dispozici n&aacute;poje a drobn&eacute; občerstven&iacute; (su&scaron;enky, chipsy).</p>
+<p>V neděli se můžete &uacute;častnit posledn&iacute;ho, <strong>dopoledn&iacute;ho bloku larpů</strong> a pot&eacute; dle libosti vyrazit domů. Nebo, pokud se v&aacute;m PIVKo bude l&iacute;bit, si můžete naj&iacute;t je&scaron;tě chvilku voln&eacute;ho času, dorazit na Seneč&aacute;k, povědět n&aacute;m jak jste si leto&scaron;n&iacute; ročn&iacute;k užili a co byste chtěli do př&iacute;&scaron;tě změnit - a třeba n&aacute;m i maličko pomoct s ukl&iacute;dem. :)</p>
+<p><strong>V r&aacute;mci festivalu v&aacute;s zveme na p&aacute;tečn&iacute; a sobotn&iacute; OFFPROGRAM:</strong></p>
+<p><strong>V p&aacute;tek </strong>večer proběhne od <strong>21.30 </strong>vystoupen&iacute;<strong> muzikanta Laega na Seneč&aacute;ku.</strong></p>
+<p><strong>V</strong> <strong>sobotu </strong>bude pro n&aacute;s od <strong>20.00</strong> otevřen&yacute; klub<strong> U star&eacute;ho psa</strong> v centru města (<a href="https://www.facebook.com/groups/53602423465/?fref=ts" target="_blank">Facebook "U star&eacute;ho psa"</a>). Bude zde možnost navečeřet se (hlasujte v anketě &ldquo;Sobotn&iacute; večeře&rdquo; na facebooku akce).</p>
+<p>V průběhu večera vystoup&iacute; hudebn&iacute; duo <strong>Kachna a Zodiac. </strong>Kromě toho bude celou dobu hr&aacute;t <strong>reprodukovan&aacute; muzika - potřebujete-li nutně sly&scaron;et svoji p&iacute;sničku, napi&scaron;te si o ni!</strong><br /><strong>Upozorňujeme, že prostory klubu budou po celou dobu př&iacute;sně NEKUŘ&Aacute;CK&Eacute;!</strong></p>
 ';
 
 $text_cena = '<p><strong>Cena jednoho larpu</strong> se bude pohybovat mezi <strong>100 &nbsp;̶ &nbsp;250,- Kč</strong>.</p>
@@ -34,6 +55,7 @@ $text_cena = '<p><strong>Cena jednoho larpu</strong> se bude pohybovat mezi <str
     <p><strong>Totem</strong><strong>&nbsp;&nbsp;&nbsp; </strong><strong>&nbsp;&nbsp;&nbsp; </strong><strong>Bez sn&iacute;daně: 65,- Kč</strong></p>
     <p><strong>Se sn&iacute;dan&iacute;: 80,- Kč</strong></p>
 </span>
+<p><strong>Informace o platbách najdete na svých účtech v sekci “Můj program”.</strong></p>
 ';
 
 
